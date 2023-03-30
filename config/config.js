55b2a2dcd208738b791b261767c95c9add8b5e41
George P <george@popow.pw>
2023-03-30 22:46:49 +0400

    [WIP] Remove unused stuff and prepare for re-initialization

--- config/config.js
+++ config/config.js
@@ -4,11 +4,6 @@ dotenv.config({ path: __dirname + "/../.env"});
 
 module.exports = {
     config: {
-        bscApiKey: process.env.BSCSCAN_API_KEY,
-        privateKey: process.env.PRIVATE_KEY,
-        airdropContractAddress: process.env.AIRDROP_CONTRACT_ADDRESS,
-        swychAddress: process.env.SWYCH_ADDRESS,
-        bscRpc: process.env.BSC_MAINNET_URL,
-        balanceFetcher: process.env.BALANCE_FETCHER_ADDRESS
+        bscApiKey: process.env.BSCSCAN_API_KEY
     }
 };
